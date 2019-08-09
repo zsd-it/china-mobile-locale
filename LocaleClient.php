326f5c729473c85acbb86568ed05ab0e29f858fc
Zsd <zhaoshoudong@qianshengqian.com>
2019-08-09 18:28:33 +0800

    初始化项目 加入sql文件

---
+++ LocaleClient.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zsd
+ * Date: 2019/8/9
+ * Time: 下午6:02
+ */
+
+namespace MobileLocale;
+
+class LocaleClient
+{
+
+}
---
+++ LocaleService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zsd
+ * Date: 2019/8/9
+ * Time: 下午6:01
+ */
+
+namespace MobileLocale;
+
+class LocaleService
+{
+
+}
